892d85857a35d3be7f6710d722b5e7ddd444b377
waimea-twatkinson2 <twatkinson2@waimea.school.nz>
2025-03-20 12:25:07 +1300

    pleeaesesesese workk nextn timem

--- src/Main.kt
+++ src/Main.kt
@@ -14,8 +14,28 @@
  * =====================================================================
  */
 
-
-
 fun main() {
-    println("Hello World!")
+    //All lists for the grid
+    val row = mutableListOf<Int>()
+    val column0 = mutableListOf<Int>()
+    val column1 = mutableListOf<Int>()
+    val column2 = mutableListOf<Int>()
+    val column3 = mutableListOf<Int>()
+    val column4 = mutableListOf<Int>()
+    val column5 = mutableListOf<Int>()
+    val column6 = mutableListOf<Int>()
+    val column7 = mutableListOf<Int>()
+    val column8 = mutableListOf<Int>()
+    val column9 = mutableListOf<Int>()
+    column1.add(1)
+    //Welcome screen
+    println("~".col("#FF0000") + "~".col("#FF1A00") +"~".col("#FF3300") +"~".col("#FF4D00") +"~".col("#FF6600") +"~".col("#FF8000") +"~".col("#FF9900") +"~".col("#FFB200") +"~".col("#FFCC00") +"~".col("#FFE600") +"~".col("#FFFF00") +"~".col("#E6FF00") +"~".col("#CCFF00") +"~".col("#B2FF00") +"~".col("#99FF00") +"~".col("#80FF00") +"~".col("#66FF00") +"~".col("#4DFF00") +"~".col("#33FF00") +"~".col("#1AFF00") +"~".col("#00FF00") +"~".col("#00FF1A") +"~".col("#00FF33") +"~".col("#00FF4D") +"~".col("#00FF66") +"~".col("#00FF80") +"~".col("#00FF99") +"~".col("#00FFB2") +"~".col("#00FFCC") +"~".col("#00FFFF"))
+    println("Welcome to Snakes and Ladders!".col("#FFFFFF"))
+    println("~".col("#00FFFF") + "~".col("#00FFCC") +"~".col("#00FFB2") +"~".col("#00FF99") +"~".col("#00FF80") +"~".col("#00FF66") +"~".col("#00FF4D") +"~".col("#00FF33") +"~".col("#00FF1A") +"~".col("#00FF00") +"~".col("#1AFF00") +"~".col("#33FF00") +"~".col("#4DFF00") +"~".col("#66FF00") +"~".col("#80FF00") +"~".col("#99FF00") +"~".col("#B2FF00") +"~".col("#CCFF00") +"~".col("#E6FF00") +"~".col("#FFFF00") +"~".col("#FFE600") +"~".col("#FFCC00") +"~".col("#FFB200") +"~".col("#FF9900") +"~".col("#FF8000") +"~".col("#FF6600") +"~".col("#FF4D00") +"~".col("#FF3300") +"~".col("#FF1A00") +"~".col("#FF0000"))
+
+    //Adding to the lists
+    row.add(2)
+
+
 }
+
